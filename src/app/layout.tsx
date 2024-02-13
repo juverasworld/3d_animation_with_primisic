3713a680d99fc97d6a504f215bb455abaf1512f6
Vee <ifebuchejulia@gmail.com>
2024-02-13 07:55:56 +0100

    added the hero page and installed gsap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import {  Syne } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const syne = Syne({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="bg-slate-900">
+      <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Syne:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+      </Head>
+      <body className={syne.className}>{children}</body>
     </html>
   );
 }
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,22 +1,39 @@
+import { KeyTextField } from "@prismicio/client";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
-/**
- * Props for `Hero`.
- */
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
-
-/**
- * Component for "Hero" Slices.
- */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const renderLetters = (name: string, key: string) => {
+    if (!name) return; // Return null if name is falsy
+    return name.split("").map((letter, index) => (
+      <span key={index} className={`name-animation name-animation-${key} inline-block opacity-0`}>
+        {letter}
+      </span>
+    ));
+  };
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <>{slice.primary.first_name}</>
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1">
+          <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
+            <span className="block text-slate-300">
+              {renderLetters(slice.primary.first_name, "first")}
+            </span>
+            <span className="-mt-[.2em] block text-slate-500">
+              
+              {renderLetters(slice.primary.last_name, "last")}</span>
+          </h1>
+          <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to bg-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl ">{slice.primary.tag_line}</span>
+        </div>
+      </div>
     </section>
+  
+  
   );
 };
 
